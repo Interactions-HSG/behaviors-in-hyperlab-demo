81c010eb0d4f46c3d59ec28aec4a71e8d3b9c8d7
Philip Junker <philipjunker@gmail.com>
2020-01-17 15:54:18 +0100

    cleanup

--- scripts/add_robot3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -e '\n\nCreating robot1 artifact...'
-curl -i -X POST \
-  http://localhost:8080/artifacts/ \
-  -H 'content-type: text/turtle' \
-  -H 'slug: robot3' \
-  -d '@payload/4_robot3_payload.ttl'
--- scripts/remove_robot1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo -e '\n\Removing robot1 artifact...'
-curl -i -X DELETE http://localhost:8080/artifacts/robot1
